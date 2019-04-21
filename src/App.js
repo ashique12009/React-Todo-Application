827ece51f1f75ae3cf67dc31f580fb61bd2ca4d3
ashique <ashique12009@gmail.com>
2019-04-21 20:23:50 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: [],
+      todoText: ""
+    }
+  }
+
+  onInputChange = e => {
+    this.setState({ todoText: e.target.value });
+  }
+
+  addTodo = () => {
+    let todoCopy = this.state.todo.slice();
+    todoCopy.push(this.state.todoText);
+    if ( this.state.todoText !== '' ) {
+      this.setState({
+        todo: todoCopy,
+        todoText: ""
+      });
+    }
+  }
+
+  enterKeyPressHandler = (e) => {
+    if ( e.key === 'Enter' ) {
+      let todoCopy = this.state.todo.slice();
+      todoCopy.push(this.state.todoText);
+      if ( this.state.todoText !== '' ) {
+        this.setState({
+          todo: todoCopy,
+          todoText: ""
+        });
+      }
+    }
+  }
+
   render() {
+    let myTodos = this.state.todo.map((e, i) => {
+      return(
+        <li className="list-group-item" key={i}>{e}</li>
+      );
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <h1>Welcome to Todo Application</h1>
+            <div className="form-group">
+              <label>Todo Title</label>
+              <input type="text" onKeyPress={ this.enterKeyPressHandler } onChange={ this.onInputChange } value={ this.state.todoText } className="form-control" placeholder="Enter Todo" />
+            </div>
+            <button className="btn btn-primary" onClick={ this.addTodo }>Add Todo</button>
+            { ( this.state.todo.length === 0 ) ? <h2>No Todos Found!</h2> : <ul className="clist list-group mt-10">{ myTodos }</ul> }
+        </div>
       </div>
     );
   }
